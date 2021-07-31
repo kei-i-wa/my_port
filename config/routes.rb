e11895070ddae61a8648d18e6141fb8c1aeba72a
kei-i-wa <kei.kumarg@gmail.com>
2021-07-31 01:48:07 +0000

    ADDコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'departments/index'
+  get 'departments/create'
+  get 'departments/edit'
+  get 'departments/update'
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+  resources :posts
   devise_for :users
   root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
