2fe1dd5ade1f0dde73bde7fec1848b06aa99321a
kei-i-wa <kei.kumarg@gmail.com>
2021-07-31 06:21:39 +0000

    UPDATEヘッダー、ログイン画面レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'departments/index'
   get 'departments/create'
   get 'departments/edit'
   get 'departments/update'
-  get 'users/show'
-  get 'users/index'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
   resources :posts
-  devise_for :users
+  resources :users, only: [:show,:index,:edit,:update,:destroy]
+  
   root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
