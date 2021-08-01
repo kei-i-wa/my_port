a6697f69b95b6a2ff69f6f23cd7fcb9b021e9a98
kei-i-wa <kei.kumarg@gmail.com>
2021-08-01 10:43:02 +0000

    ADD記事の削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,45 +14,41 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id=current_user.id
     if @post.save
-      redirect_to posts_path(@post),notice:'You have created book successfully.'
+      redirect_to posts_path(@post),notice:'投稿完了しました:)'
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    # respond_to do |format|
-    #   if @post.update(post_params)
-    #     format.html { redirect_to @post, notice: "Post was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @post }
-    #   else
-    #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @post.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id),notice:'投稿完了しました:)'
+    else
+      render:edit
+    end
   end
 
-  # DELETE /posts/1 or /posts/1.json
+  
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+   
     def post_params
       params.require(:post).permit(:title, :content)
     end
