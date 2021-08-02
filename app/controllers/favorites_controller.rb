a4d72497b75e5ef4fea15dc6a05d061ddb105690
kei-i-wa <kei.kumarg@gmail.com>
2021-08-02 01:44:17 +0000

    UPDATEいいねの非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,20 @@
 class FavoritesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
     # お気に入りに紐づく投稿取得
     post=Post.find(params[:post_id])
     favorite=current_user.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to request.referer
+    # 非同期通信のため以下削除/@post上部追記
+    # redirect_to request.referer
+    
   end
   
   def destroy
+    @post = Post.find(params[:post_id])
     post=Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: post.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end
