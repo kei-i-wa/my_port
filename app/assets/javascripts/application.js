4b401a29d5e444fa88e32fb061c95b1353b5e6fa
kei-i-wa <kei.kumarg@gmail.com>
2021-08-11 09:46:03 +0000

    ADDトップページ下部

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,8 +31,20 @@
 // テストのために記述
 
 
-$(document).ready(function () {
-  $('.jquery').on('click', function(){
-    $(this).css('color','red');
-  });
-});
+// $(document).ready(function () {
+//   $('.jquery').on('click', function(){
+//     $(this).css('color','red');
+//   });
+// });
+
+// /*アニメーションターゲット取得し、画面上に見えたらアニメーションする*/
+const targetElement = document.querySelectorAll(".animationTarget");
+// console.log(targetElement);
+// 上からのきょりようし自身の高さを取得し、今どれくらいかを判断
+for (let i = 0; i < targetElement.length; i++){
+  const getElementDistance = targetElement[i].getBoundingClientRect().top
+  // これで上からの距離をとる
+  // いま見えてるとこから、その要素の画像の上までの距離
+  
+  console.log(getElementDistance);
+}
