02fa05eac515e6d2f8071b9ce872c26d3393f404
kei-i-wa <kei.kumarg@gmail.com>
2021-08-16 05:34:57 +0000

    UPDATEメール本番環境対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = {  :host => 'http://18.181.166.165'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['KEY'],
+    password: ENV['SECRET_KEY'],
+    domain: 'gmail.com',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true }
+end
 end
