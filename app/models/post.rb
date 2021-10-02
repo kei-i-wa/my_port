0144d193cd01fa81a1133b85f5e5006f44ce7057
kei-i-wa <kei.kumarg@gmail.com>
2021-10-02 12:50:12 +0000

    UPDATE投稿数のグラフのリファクタリング

--- app/models/post.rb
+++ app/models/post.rb
@@ -48,17 +48,7 @@ class Post < ApplicationRecord
     where('title LIKE(?)', "%#{search}%").or(where('content LIKE(?)', "%#{search}%"))
   end
 
-  # 過去7日間の投稿数表示
-  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
-  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
-  scope :created_2days_ago, -> { where(created_at: 2.days.ago.all_day) }
-  scope :created_3days_ago, -> { where(created_at: 3.days.ago.all_day) }
-  scope :created_4days_ago, -> { where(created_at: 4.days.ago.all_day) }
-  scope :created_5days_ago, -> { where(created_at: 5.days.ago.all_day) }
-  scope :created_6days_ago, -> { where(created_at: 6.days.ago.all_day) }
-
-  # scope :created_this_1week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
-  # scope :created_this_week, -> { where(created_at: Time.zone.now.prev_week(:monday)..Time.zone.now.prev_week(:friday)) }
+  scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
 
   # 通知のアソシエーション
   has_many :notifications, dependent: :destroy
