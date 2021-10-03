4be7985a801d4ef09a4e18bff031f069395361f8
kei-i-wa <kei.kumarg@gmail.com>
2021-10-03 01:02:30 +0000

    UPDATEいいねグラフのリファクタリング

--- app/models/point.rb
+++ app/models/point.rb
@@ -3,11 +3,6 @@ class Point < ApplicationRecord
   belongs_to :giver, class_name: 'User', foreign_key: 'giver_id', optional: true
   belongs_to :getter, class_name: 'User', foreign_key: 'getter_id', optional: true
 
-  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
-  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
-  scope :created_2days_ago, -> { where(created_at: 2.days.ago.all_day) }
-  scope :created_3days_ago, -> { where(created_at: 3.days.ago.all_day) }
-  scope :created_4days_ago, -> { where(created_at: 4.days.ago.all_day) }
-  scope :created_5days_ago, -> { where(created_at: 5.days.ago.all_day) }
-  scope :created_6days_ago, -> { where(created_at: 6.days.ago.all_day) }
+  scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
+
 end
