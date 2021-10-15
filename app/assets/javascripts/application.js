449c9bb2a8e76783213a0c872bb7f32ec148d6b9
kei-i-wa <kei.kumarg@gmail.com>
2021-10-15 05:34:28 +0000

    UPDATEお天気ページ画像追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -110,6 +110,9 @@ document.addEventListener("turbolinks:load", function() {
       $('#speed').text(data.wind.speed);
       // 天気
       $('#weather').text(data.weather[0].main);
+            // 天気アイコン
+      $('#img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('#img').attr("alt",data.weather[0].main);
     }).fail(function (data) {
       //通信失敗
       alert('通信に失敗しました。');      });
